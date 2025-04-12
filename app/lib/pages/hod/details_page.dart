ac16ea21c82bef94c86d53f639e14900100e1aa5
MathewsVinoy <mathewsvinoy@gmail.com>
2025-04-13 00:31:58 +0530

    update the ui to this page

--- app/lib/pages/hod/details_page.dart
+++ app/lib/pages/hod/details_page.dart
@@ -2,6 +2,8 @@ import 'package:app/database/login.dart';
 import 'package:app/database/outpass.dart';
 import 'package:app/functions/accpect_reject.dart';
 import 'package:app/pages/hod/nav_bar_hod.dart';
+import 'package:app/ui/custom_appbar.dart';
+import 'package:app/ui/details_container.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPageHod extends StatelessWidget {
@@ -16,87 +18,22 @@ class DetailsPageHod extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('OutPass Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image.network(
-                    'http://mbccet.com/img_small/${outPass.admno}.jpg', // Replace with actual image URL
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) =>
-                        const Icon(Icons.error, size: 150),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text("Name: ${outPass.name}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Admission No: ${outPass.admno}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Semester: ${outPass.sem}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Department: ${outPass.dept}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Reason: ${outPass.reason}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Hostel: ${outPass.hostel}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Number of Days: ${outPass.noDays}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Start Date: ${outPass.startDate}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("End Date: ${outPass.endDate}",
-                  style: const TextStyle(fontSize: 18)),
-              Text("Working Day: ${outPass.workDay}",
-                  style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                      onPressed: () async {
-                        await AccpectReject().acceptedFun("HOD", outPass.admno);
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => NavBarHod(data: data)));
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green),
-                      child: Text(
-                        "Accepet",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                  ElevatedButton(
-                      onPressed: () async {
-                        await AccpectReject().rejectFun("HOD", outPass.admno);
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => NavBarHod(data: data)));
-                      },
-                      style:
-                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                      child: Text(
-                        "Reject",
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ],
-              )
-            ],
-          ),
+        appBar: customAppbar(
+          'OutPass Details',
         ),
-      ),
-    );
+        body: DetailsContainer(
+          outPass: outPass,
+          acccpectBtn: () async {
+            await AccpectReject().acceptedFun("HOD", outPass.admno);
+
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => NavBarHod(data: data)));
+          },
+          rejuctBtn: () async {
+            await AccpectReject().rejectFun("HOD", outPass.admno);
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => NavBarHod(data: data)));
+          },
+        ));
   }
 }
