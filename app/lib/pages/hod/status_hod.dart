df6d88e368b32f8b3f423e0f87d2bd6fe240baf2
MathewsVinoy <mathewsvinoy@gmail.com>
2025-04-14 10:48:47 +0530

    design compleated

--- app/lib/pages/hod/status_hod.dart
+++ app/lib/pages/hod/status_hod.dart
@@ -1,6 +1,11 @@
 import 'package:app/database/login.dart';
 import 'package:app/database/outpass.dart';
 import 'package:app/functions/get_queary.dart';
+import 'package:app/ui/color.dart';
+import 'package:app/ui/custom_appbar.dart';
+import 'package:app/ui/errormsg.dart';
+import 'package:app/ui/on_data.dart';
+import 'package:app/ui/status_test.dart';
 import 'package:flutter/material.dart';
 
 class StatusHod extends StatefulWidget {
@@ -23,25 +28,61 @@ class _StatusHodState extends State<StatusHod> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: customAppbar("History"),
       body: FutureBuilder<List<OutPass>>(
         future: futureOutPass,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
+            return errormsg(snapshot.error.toString());
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No data available'));
+            return onData();
           } else {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 OutPass outPass = snapshot.data![index];
                 return Card(
-                  child: ListTile(
-                    title: Text('ID: ${outPass.admno}'),
-                    subtitle:
-                        Text('Name: ${outPass.name}\nDate: ${outPass.endDate}'),
+                  color: white,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Name: ${outPass.name}',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: black,
+                              ),
+                            ),
+                            statusText('', outPass.staffStatus),
+                          ],
+                        ),
+                        Text(
+                          'Adm No: ${outPass.admno}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: black,
+                          ),
+                        ),
+                        Text(
+                          'Date: ${outPass.startDate} - ${outPass.endDate}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: black,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
